cb5e30566969455950ff564e33da306ee15c1f9b
Zach Newell <new14008@byui.edu>
2018-02-26 13:40:48 -0700

    2/26inclass

--- app/src/main/java/com/example/team6/togo/HomeActivity.java
+++ app/src/main/java/com/example/team6/togo/HomeActivity.java
@@ -1,7 +1,10 @@
 package com.example.team6.togo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void toMenu(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        startActivity(intent);
     }
 }
