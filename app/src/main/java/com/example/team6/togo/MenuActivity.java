3e2792022213f789d7c54e3932f50507886d12a3
Andrew Shore <andrewshore1414@gmail.com>
2018-03-02 12:29:16 -0700

    colors and toolbar try

--- app/src/main/java/com/example/team6/togo/MenuActivity.java
+++ app/src/main/java/com/example/team6/togo/MenuActivity.java
@@ -3,21 +3,40 @@ package com.example.team6.togo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MenuActivity extends AppCompatActivity {
 
+//    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
         Intent intent = getIntent();
+
+
     }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        getMenuInflater().inflate(R.menu.menu, menu);
+//        return true;
+//    }
 
     public void toCheckout(View view) {
         Intent intent = new Intent(this, CheckoutActivity.class);
         startActivity(intent);
+    }
+
+    public void toToast(View view) {
+        Toast.makeText(this, "I clicked", Toast.LENGTH_SHORT).show();
     }
 }
