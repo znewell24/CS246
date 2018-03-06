f9f9b1126408894a520fafee5a4baab499bed535
Justin <ree15008@byui.edu>
2018-03-05 19:35:27 -0700

    Working Menu & Edited Main Page

--- app/src/main/java/com/example/team6/togo/HomeActivity.java
+++ app/src/main/java/com/example/team6/togo/HomeActivity.java
@@ -3,23 +3,16 @@ package com.example.team6.togo;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
 
 
 public class HomeActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
     }
-
-
 
     public void toMenu(View view) {
         Intent intent = new Intent(this, MenuActivity.class);
