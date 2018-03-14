984c19228d3356ec6ae229df6b1844b6629d91f9
Zach Newell <new14008@byui.edu>
2018-03-14 12:38:58 -0600

    cart.javaandfood.java

--- app/src/main/java/com/example/team6/togo/DisplayItemActivity.java
+++ app/src/main/java/com/example/team6/togo/DisplayItemActivity.java
@@ -5,10 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.jar.Attributes;
 
 public class DisplayItemActivity extends AppCompatActivity {
+
+    private TextView tName;
+    private TextView tDescription;
+    private TextView tPrice;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,9 +25,9 @@ public class DisplayItemActivity extends AppCompatActivity {
         String description = intent.getStringExtra(MenuActivity.DES);
         String price = intent.getStringExtra(MenuActivity.PRICE);
 
-        TextView tName = findViewById(R.id.name);
-        TextView tDescription = findViewById(R.id.description);
-        TextView tPrice = findViewById(R.id.price);
+        tName = findViewById(R.id.name);
+        tDescription = findViewById(R.id.description);
+        tPrice = findViewById(R.id.price);
 
         tName.setText(name);
         tDescription.setText(description);
@@ -33,6 +38,19 @@ public class DisplayItemActivity extends AppCompatActivity {
     public void added(View view) {
         Intent intent = new Intent(this, MenuActivity.class);
         startActivity(intent);
-    }
 
+        String n = tName.getText().toString();
+        String p = tPrice.getText().toString();
+        TextView quantity = findViewById(R.id.quantity);
+        int q = Integer.parseInt(quantity.getText().toString());
+        Food f = new Food(n, p, q);
+
+        double dPrice = Double.parseDouble(p);
+        for (int i = 0 ; i < q; i++) {
+            ((Cart) this.getApplication()).setTotal(dPrice);
+        }
+
+        ((Cart) this.getApplication()).setCartList(f);
+        Toast.makeText(this, "Item Added to Cart", Toast.LENGTH_SHORT).show();
+    }
 }
--- app/src/main/java/com/example/team6/togo/Food.java
+++ app/src/main/java/com/example/team6/togo/Food.java
@@ -1,18 +1,44 @@
 package com.example.team6.togo;
 
+import android.content.Intent;
+
 /**
- * Created by andrew on 2/28/2018.
+ * Created by znewe_000 on 3/14/2018.
  */
 
-public interface Food {
+public class Food {
+    private String name;
+    private String price;
+    private int quantity;
 
-    String getName();
-    void setName(String name);
+    public Food (String n, String p, int q) {
+        name = n;
+        price = p;
+        quantity = q;
+    }
 
-    String getDescription();
-    void setDescription(String description);
 
-    double getPrice();
-    void setPrice(double price);
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
