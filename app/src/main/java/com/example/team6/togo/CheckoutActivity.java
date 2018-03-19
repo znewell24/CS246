c89b183b0ba2a931bf80f37c72516e4f00857e30
Zach Newell <new14008@byui.edu>
2018-03-19 13:27:28 -0600

    checkoutactivity

--- app/src/main/java/com/example/team6/togo/CheckoutActivity.java
+++ app/src/main/java/com/example/team6/togo/CheckoutActivity.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ScrollView;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class CheckoutActivity extends AppCompatActivity {
 
@@ -15,6 +18,28 @@ public class CheckoutActivity extends AppCompatActivity {
         setContentView(R.layout.activity_checkout);
 
         Intent intent = getIntent();
+
+        createReciept();
+    }
+
+    public void createReciept () {
+        List<Food> l = ((Cart) this.getApplication()).getCartList();
+
+        final LinearLayout li = (LinearLayout)findViewById(R.id.ll);
+        for (Food f: l) {
+            final TextView n = new TextView(this);
+            n.setText(f.getName());
+            li.addView(n);
+
+
+            TextView p = new TextView(this);
+            p.setText(f.getPrice());
+            li.addView(p);
+
+            TextView q = new TextView(this);
+            q.setText(f.getQuantity());
+            li.addView(q);
+        }
 
 
     }
--- app/src/main/java/com/example/team6/togo/Food.java
+++ app/src/main/java/com/example/team6/togo/Food.java
@@ -24,6 +24,6 @@ public class Food {
     public void setName(String name) { this.name = name; }
     public String getPrice() { return price; }
     public void setPrice(String price) { this.price = price; }
-    public int getQuantity() { return quantity; }
+    public String getQuantity() { return Integer.toString(quantity); }
     public void setQuantity(int quantity) { this.quantity = quantity; }
 }
